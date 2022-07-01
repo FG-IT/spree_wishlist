8d36ae00348493f7b1f23002dacf30aff1a6d035
JX <johnnyxiang2017@gmail.com>
2022-07-01 15:09:04 -0400

    bug

--- app/controllers/spree/wished_products_controller.rb
+++ app/controllers/spree/wished_products_controller.rb
@@ -3,16 +3,18 @@ class Spree::WishedProductsController < Spree::StoreController
   respond_to :html
 
   def create
-    @wished_product = Spree::WishedProduct.new(wished_product_attributes)
+
     @wishlist = spree_current_user.wishlist
 
     if @wishlist.include? params[:wished_product][:variant_id]
       @wished_product = @wishlist.wished_products.detect { |wp| wp.variant_id == params[:wished_product][:variant_id].to_i }
+      @wished_product.quantity = params[:wished_product][:quantity]
     else
-      @wished_product.wishlist = spree_current_user.wishlist
+      @wished_product = Spree::WishedProduct.new(wished_product_attributes)
+      @wished_product.wishlist = @wishlist 
       @wished_product.save
     end
-
+   
     respond_with(@wished_product) do |format|
       format.html { redirect_to wishlist_url(@wishlist) }
     end
@@ -39,6 +41,6 @@ class Spree::WishedProductsController < Spree::StoreController
   private
 
   def wished_product_attributes
-    params.require(:wished_product).permit(:variant_id, :remark, :quantity)
+    params.require(:wished_product).permit(:variant_id, :quantity)
   end
 end
